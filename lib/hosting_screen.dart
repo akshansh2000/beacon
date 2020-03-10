ce0776a386d9429eda4c496848039b72d04e55d4
Akshansh Bhanjana <akshansh2000@gmail.com>
2020-03-11 04:01:32 +0530

    actual location uploads to firebase

--- lib/hosting_screen.dart
+++ lib/hosting_screen.dart
@@ -4,6 +4,7 @@ import 'package:beacon/common_screen.dart';
 
 import 'package:firebase_database/firebase_database.dart';
 import 'package:random_string/random_string.dart';
+import 'package:location/location.dart';
 
 class HostingScreen extends StatefulWidget {
   HostingScreen({Key key}) : super(key: key);
@@ -15,28 +16,49 @@ class HostingScreen extends StatefulWidget {
 class _HostingScreenState extends State<HostingScreen> {
   DatabaseReference _databaseReference;
   bool _isHosted = false;
+  Size size;
 
   @override
   void initState() {
     super.initState();
 
     _databaseReference = FirebaseDatabase.instance.reference();
-    hostDatabase();
+    getLocation();
   }
 
-  hostDatabase() async {
-    _databaseReference.child(randomString(15)).set({
-      "lat": 23,
-      "lon": 23,
+  getLocation() async {
+    final _locationData = await Location().getLocation();
+    hostLocation(_locationData);
+  }
+
+  hostLocation(locationData) async {
+    final _randomString = randomAlphaNumeric(15);
+    _databaseReference.child(_randomString).set({
+      "lat": locationData.latitude,
+      "lon": locationData.longitude,
+    });
+
+    setState(() {
+      _isHosted = true;
     });
   }
 
   @override
   Widget build(BuildContext context) {
+    size = MediaQuery.of(context).size;
+
     return CommonScreen(
       tag: "host",
       color: Colors.blueAccent,
-      child: _isHosted ? Container() : CircularProgressIndicator(),
+      child: _isHosted
+          ? Container()
+          : Center(
+              child: Container(
+                width: size.width / 5,
+                height: size.width / 5,
+                child: CircularProgressIndicator(),
+              ),
+            ),
     );
   }
 }
