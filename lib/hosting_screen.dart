ee1939b29b4da1b47fcd465cd4c1ecd3aa9cfc9d
Akshansh Bhanjana <akshansh2000@gmail.com>
2020-03-11 03:42:58 +0530

    added testing coordinates

--- lib/hosting_screen.dart
+++ lib/hosting_screen.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 
 import 'package:beacon/common_screen.dart';
 
+import 'package:firebase_database/firebase_database.dart';
+import 'package:random_string/random_string.dart';
+
 class HostingScreen extends StatefulWidget {
   HostingScreen({Key key}) : super(key: key);
 
@@ -10,11 +13,30 @@ class HostingScreen extends StatefulWidget {
 }
 
 class _HostingScreenState extends State<HostingScreen> {
+  DatabaseReference _databaseReference;
+  bool _isHosted = false;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _databaseReference = FirebaseDatabase.instance.reference();
+    hostDatabase();
+  }
+
+  hostDatabase() async {
+    _databaseReference.child(randomString(15)).set({
+      "lat": 23,
+      "lon": 23,
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return CommonScreen(
       tag: "host",
       color: Colors.blueAccent,
+      child: _isHosted ? Container() : CircularProgressIndicator(),
     );
   }
 }
