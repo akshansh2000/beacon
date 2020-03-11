07fabf2fa00fe6f395f117b471f8759cd309060c
Akshansh Bhanjana <akshansh2000@gmail.com>
2020-03-11 07:21:04 +0530

    added pop scope

--- lib/tracking_screen.dart
+++ lib/tracking_screen.dart
@@ -38,162 +38,168 @@ class _TrackingScreenState extends State<TrackingScreen> {
   Widget build(BuildContext context) {
     size = MediaQuery.of(context).size;
 
-    return CommonScreen(
-      tag: "track",
-      color: Colors.redAccent,
-      child: isLoaded
-          ? Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: <Widget>[
-                Text(
-                  "TRACKING\nBEACON",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 30,
+    return WillPopScope(
+      onWillPop: () {
+        showWarning(context, "track");
+        return null;
+      },
+      child: CommonScreen(
+        tag: "track",
+        color: Colors.redAccent,
+        child: isLoaded
+            ? Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: <Widget>[
+                  Text(
+                    "TRACKING\nBEACON",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 30,
+                    ),
                   ),
-                ),
-                Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: <Widget>[
-                    Padding(
-                      padding: EdgeInsets.all(10),
-                      child: Icon(
-                        Icons.location_on,
-                        size: 30,
+                  Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      Padding(
+                        padding: EdgeInsets.all(10),
+                        child: Icon(
+                          Icons.location_on,
+                          size: 30,
+                        ),
                       ),
-                    ),
-                    Text(
-                      "${lat.toStringAsFixed(4)}, "
-                      "${lon.toStringAsFixed(4)}",
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 24,
-                      ),
-                    ),
-                  ],
-                ),
-                Column(
-                  mainAxisSize: MainAxisSize.min,
-                  children: <Widget>[
-                    Padding(
-                      padding: EdgeInsets.all(10),
-                      child: Text(
-                        "Open in Maps",
+                      Text(
+                        "${lat.toStringAsFixed(4)}, "
+                        "${lon.toStringAsFixed(4)}",
                         style: TextStyle(
                           fontWeight: FontWeight.bold,
                           fontSize: 24,
                         ),
                       ),
-                    ),
-                    IconButton(
-                      icon: Icon(Icons.arrow_forward_ios),
-                      iconSize: 26,
-                      onPressed: () async {
-                        final urlString =
-                            "https://www.google.com/maps/search/?api=1&query=$lat,$lon";
-                        if (await canLaunch(urlString))
-                          await launch(urlString);
-                        else
-                          throw "Could not load map";
-                      },
-                    ),
-                  ],
-                ),
-              ],
-            )
-          : isLoading
-              ? Center(
-                  child: Container(
-                    width: size.width / 5,
-                    height: size.width / 5,
-                    child: CircularProgressIndicator(),
+                    ],
                   ),
-                )
-              : Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: <Widget>[
-                    Text(
-                      "ENTER YOUR\nFRIEND'S ID",
-                      textAlign: TextAlign.center,
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 30,
-                        color: Colors.white,
-                        decoration: TextDecoration.none,
-                        fontFamily: "Roboto",
+                  Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: <Widget>[
+                      Padding(
+                        padding: EdgeInsets.all(10),
+                        child: Text(
+                          "Open in Maps",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 24,
+                          ),
+                        ),
                       ),
+                      IconButton(
+                        icon: Icon(Icons.arrow_forward_ios),
+                        iconSize: 26,
+                        onPressed: () async {
+                          final urlString =
+                              "https://www.google.com/maps/search/?api=1&query=$lat,$lon";
+                          if (await canLaunch(urlString))
+                            await launch(urlString);
+                          else
+                            throw "Could not load map";
+                        },
+                      ),
+                    ],
+                  ),
+                ],
+              )
+            : isLoading
+                ? Center(
+                    child: Container(
+                      width: size.width / 5,
+                      height: size.width / 5,
+                      child: CircularProgressIndicator(),
                     ),
-                    SizedBox(height: 100),
-                    Center(
-                      child: Material(
-                        color: Colors.transparent,
-                        child: Padding(
-                          padding: EdgeInsets.symmetric(horizontal: 20),
-                          child: TextFormField(
-                            textAlign: TextAlign.center,
-                            style: TextStyle(fontSize: 24),
-                            controller: _textEditingController,
-                            decoration: InputDecoration(
-                              border: OutlineInputBorder(
-                                borderRadius: BorderRadius.circular(15),
+                  )
+                : Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                        "ENTER YOUR\nFRIEND'S ID",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 30,
+                          color: Colors.white,
+                          decoration: TextDecoration.none,
+                          fontFamily: "Roboto",
+                        ),
+                      ),
+                      SizedBox(height: 100),
+                      Center(
+                        child: Material(
+                          color: Colors.transparent,
+                          child: Padding(
+                            padding: EdgeInsets.symmetric(horizontal: 20),
+                            child: TextFormField(
+                              textAlign: TextAlign.center,
+                              style: TextStyle(fontSize: 24),
+                              controller: _textEditingController,
+                              decoration: InputDecoration(
+                                border: OutlineInputBorder(
+                                  borderRadius: BorderRadius.circular(15),
+                                ),
                               ),
                             ),
                           ),
                         ),
                       ),
-                    ),
-                    SizedBox(height: 50),
-                    IconButton(
-                      icon: Icon(Icons.check),
-                      iconSize: 30,
-                      onPressed: () {
-                        final text = _textEditingController.value.text;
-                        if (text == null ||
-                            !RegExp("^[a-z|A-Z|\\d]{15}\$").hasMatch(text))
-                          scaffoldState.currentState.showSnackBar(
-                            SnackBar(
-                              backgroundColor: Colors.grey[900],
-                              content: Text(
-                                "Please enter a valid ID.",
-                                style: TextStyle(
-                                  color: Colors.white,
+                      SizedBox(height: 50),
+                      IconButton(
+                        icon: Icon(Icons.check),
+                        iconSize: 30,
+                        onPressed: () {
+                          final text = _textEditingController.value.text;
+                          if (text == null ||
+                              !RegExp("^[a-z|A-Z|\\d]{15}\$").hasMatch(text))
+                            scaffoldState.currentState.showSnackBar(
+                              SnackBar(
+                                backgroundColor: Colors.grey[900],
+                                content: Text(
+                                  "Please enter a valid ID.",
+                                  style: TextStyle(
+                                    color: Colors.white,
+                                  ),
                                 ),
                               ),
-                            ),
-                          );
-                        else {
-                          _databaseReference
-                              .child(_textEditingController.value.text)
-                              .once()
-                              .then(
-                            (DataSnapshot data) {
-                              if (data.value == null)
-                                scaffoldState.currentState.showSnackBar(
-                                  SnackBar(
-                                    backgroundColor: Colors.grey[900],
-                                    content: Text(
-                                      "Looks like no one is sharing their location with this ID, yet. Are you sure you entered the correct ID?",
-                                      style: TextStyle(
-                                        color: Colors.white,
+                            );
+                          else {
+                            _databaseReference
+                                .child(_textEditingController.value.text)
+                                .once()
+                                .then(
+                              (DataSnapshot data) {
+                                if (data.value == null)
+                                  scaffoldState.currentState.showSnackBar(
+                                    SnackBar(
+                                      backgroundColor: Colors.grey[900],
+                                      content: Text(
+                                        "Looks like no one is sharing their location with this ID, yet. Are you sure you entered the correct ID?",
+                                        style: TextStyle(
+                                          color: Colors.white,
+                                        ),
                                       ),
                                     ),
-                                  ),
-                                );
-                              else {
-                                setState(() {
-                                  isLoaded = true;
-                                  lat = data.value["lat"];
-                                  lon = data.value["lon"];
-                                });
-                              }
-                            },
-                          );
-                        }
-                      },
-                    ),
-                  ],
-                ),
+                                  );
+                                else {
+                                  setState(() {
+                                    isLoaded = true;
+                                    lat = data.value["lat"];
+                                    lon = data.value["lon"];
+                                  });
+                                }
+                              },
+                            );
+                          }
+                        },
+                      ),
+                    ],
+                  ),
+      ),
     );
   }
 }
