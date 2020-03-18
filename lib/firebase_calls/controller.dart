bbc5caae2181ea4bb9abfec63dc8993d59da407f
Akshansh Bhanjana <akshansh2000@gmail.com>
2020-03-18 16:33:10 +0530

    fixed prefs being null

--- lib/firebase_calls/controller.dart
+++ lib/firebase_calls/controller.dart
@@ -1,24 +1,21 @@
 import 'package:beacon/components/prefs.dart';
 
 import 'package:firebase_database/firebase_database.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 FirebaseController controller;
 
 class FirebaseController {
   DatabaseReference _database;
-  SharedPreferences _prefs;
 
   FirebaseController() {
     _database = FirebaseDatabase.instance.reference();
-    _prefs = prefsInstance.prefs;
   }
 
   Map createRoom(String roomId) {
     final _roomDetails = {
-      "expiry": _prefs.getString("expiry"),
-      _prefs.getString("id"): {
-        "name": _prefs.getString("name"),
+      "expiry": prefsInstance.prefs.getString("expiry"),
+      prefsInstance.prefs.getString("id"): {
+        "name": prefsInstance.prefs.getString("name"),
         "mode": "host",
       },
     };
